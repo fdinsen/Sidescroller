224d77f3811126659537c7257d34f5d6a89e0afa
Nikolaj Larsen <cph-nl174@cphbusiness.dk>
2021-02-23 19:56:23 +0100

    Level 2

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+
+    public Transform movingPlatform;
+    public Transform position1;
+    public Transform position2;
+    public Vector3 newPosition;
+    public string currentState;
+    public float smooth;
+    public float resetTime;
+
+    // Start is called before the first frame update
+    void Start(){
+        ChangeTarget ();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate(){
+        movingPlatform.position = Vector3.Lerp (movingPlatform.position, newPosition, smooth * Time.deltaTime);
+    }
+
+    void ChangeTarget() {
+        if(currentState == "Moving To Position 1") {
+            currentState = "Moving To Position 2";
+            newPosition = position2.position;
+        }
+        else if(currentState == "Moving To Position 2") {
+            currentState = "Moving To Position 1";
+            newPosition = position1.position;
+        }
+        else if(currentState == "") {
+            currentState = "Moving To Position 2";
+            newPosition = position2.position;
+        }
+        Invoke ("ChangeTarget", resetTime);
+    }
+}
